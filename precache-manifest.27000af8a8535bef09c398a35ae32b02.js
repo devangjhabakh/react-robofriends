dd6b44f2d987aa2b0d43d875b64d5bb25a931831
Devang Jhabakh Jai <devangjhabakh@berkeley.edu>
2020-06-06 14:39:09 +0400

    Updates

---
+++ precache-manifest.27000af8a8535bef09c398a35ae32b02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837b06d50cf018a31e992e5b9064b4c0",
+    "url": "/react-robofriends/index.html"
+  },
+  {
+    "revision": "3b3761d97755b5fa4ba3",
+    "url": "/react-robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "632590e901b48904db8b",
+    "url": "/react-robofriends/static/css/main.c7d2a85c.chunk.css"
+  },
+  {
+    "revision": "3b3761d97755b5fa4ba3",
+    "url": "/react-robofriends/static/js/2.b7db9bba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-robofriends/static/js/2.b7db9bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "632590e901b48904db8b",
+    "url": "/react-robofriends/static/js/main.039a2c10.chunk.js"
+  },
+  {
+    "revision": "ffc37ceeea4a5a5eccd9",
+    "url": "/react-robofriends/static/js/runtime-main.0699e157.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
