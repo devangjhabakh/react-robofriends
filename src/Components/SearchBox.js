929f4e5607c65e959113e56ea95a544fe503f4e6
Devang Jhabakh Jai <devangjhabakh@berkeley.edu>
2020-06-06 16:07:01 +0400

    Fixed awkward search box on mobile view

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -3,11 +3,11 @@ import React, {Component} from 'react';
 class SearchBox extends Component{
 	render(){
 		return(
-			<div className = "tc f1">
+			<div className = "pa2">
 				<input type="search" 
 				placeholder='search robots...' 
 				className = 'pa3 ba b--green bg-lightest-blue'
-				onChange = {this.props.searchChange}></input>
+				onChange = {this.props.searchChange} />
 			</div>
 			);
 	}
